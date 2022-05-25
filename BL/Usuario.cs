bd84f18c6ca3bcc5425645268bb1201447479717
Carmen Angélica García Pérez <cangelicagp@gmail.com>
2022-05-25 15:17:03 -0500

    Add project files.

---
+++ BL/Usuario.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Usuario
+    {
+        public static ML.Result Add(ML.Usuario usuario)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (SqlConnection context = new SqlConnection(DL.Conexion.GetConnection()))
+                {
+                    string query = "INSERT INTO [Usuario]([Nombre],[ApellidoPaterno],[ApellidoMaterno] ,[Sexo],[CURP])VALUES (@Nombre,@ApellidoPaterno,@ApellidoMaterno,@Sexo,@CURP)";
+
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = query;
+                    cmd.Connection = context;
+
+                    SqlParameter[] collection = new SqlParameter[5];
+
+                    collection[0] = new SqlParameter("@Nombre", System.Data.SqlDbType.VarChar);
+                    collection[0].Value = usuario.Nombre;
+
+                    collection[1] = new SqlParameter("@ApellidoPaterno", System.Data.SqlDbType.VarChar);
+                    collection[1].Value = usuario.ApellidoPaterno;
+
+                    collection[2] = new SqlParameter("@ApellidoMaterno", System.Data.SqlDbType.VarChar);
+                    collection[2].Value = usuario.ApellidoMaterno;
+
+                    collection[3] = new SqlParameter("@Sexo", System.Data.SqlDbType.Char);
+                    collection[3].Value = usuario.Sexo;
+
+                    collection[4] = new SqlParameter("@CURP", System.Data.SqlDbType.VarChar);
+                    collection[4].Value = usuario.CURP;
+
+                    cmd.Parameters.AddRange(collection);
+                    cmd.Connection.Open();
+
+                    int RowsAffected = cmd.ExecuteNonQuery();
+                    if (RowsAffected > 0)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+
+            }
+            return result;
+        }
+    }
+}
---
+++ PL/Usuario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    public class Usuario
+    {
+        public static void Add()
+        {
+            ML.Usuario usuario = new ML.Usuario();
+
+            Console.WriteLine("Ingrese Nombre");
+            usuario.Nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese ApellidoPaterno");
+            usuario.ApellidoPaterno = Console.ReadLine();
+
+            Console.WriteLine("Ingrese ApellidoMaterno");
+            usuario.ApellidoMaterno = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el Sexo");
+            usuario.Sexo = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el CURP");
+            usuario.CURP = Console.ReadLine();
+
+            ML.Result result = BL.Usuario.Add(usuario);
+
+            if (result.Correct)
+            {
+                Console.WriteLine("Se ha registrado la materia");
+            }
+            else
+            {
+                Console.WriteLine("No se ha podido registrar la materia" + result.ErrorMessage);
+            }
+        }
+
+
+    }
+}
